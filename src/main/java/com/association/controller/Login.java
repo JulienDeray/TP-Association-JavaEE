6102b80c3c3816f3ab0078104ef9dadf918e8375
Julien DERAY <julien.deray@serli.com>
2013-09-28 14:52:21 +0200

    Merge Login

--- src/main/java/com/association/controller/Login.java
+++ src/main/java/com/association/controller/Login.java
@@ -1,10 +1,13 @@
-package com.association;
+package com.association.controller;
 
-import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Servlet implementation class Login
